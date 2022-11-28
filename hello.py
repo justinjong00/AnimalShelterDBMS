e369636450e688d6ab5e7cc6ca72e6ed36db4dca
Justin Ong <o.justin.j@gmail.com>
2022-11-28 14:58:42 -0500

    initial commit

---
+++ hello.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Create a route decorator
+@app.route('/')
+
+#def index():
+#	return "<h1>Hello World!</h1>"
+
+def index():
+	return render_template("index.html")
+
+#localhost:5000/user/john
+@app.route('/user/<name>')
+
+def user(name):
+	return render_template("user.html", name=name)
